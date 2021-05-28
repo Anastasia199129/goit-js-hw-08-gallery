86ee7846b9afb52e7ebcc0b4db709d48dd102e75
Anastasia <nastasia199126@gmail.com>
2021-05-28 22:47:23 +0300

    commit

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,81 +1,13 @@
- const gallery = [
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-      description: 'Hokkaido Flower',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-      description: 'Container Haulage Freight',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-      description: 'Aerial Beach View',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-      description: 'Flower Blooms',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-      description: 'Alpine Mountains',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-      description: 'Mountain Lake Sailing',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-      description: 'Alpine Spring Meadows',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-      description: 'Nature Landscape',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-      description: 'Lighthouse Coast Sea',
-    },
-  ];
+ 
 
   const galleryUl = document.querySelector('.js-gallery')
-  // const galleryImg = document.querySelector('.gallery__image')
   const modal = document.querySelector('.js-lightbox')
   const modalImg = document.querySelector('.lightbox__image')
+
   const buttonClose = document.querySelector('[data-action="close-lightbox"]')
   const overlay = document.querySelector('.lightbox__overlay')
-
-
   const ArrayOriginalImgSrc = gallery.map(e => e.original)
   
-
-
   const galerryMarkup = creategalery(gallery);
   galleryUl.insertAdjacentHTML('beforeend', galerryMarkup);
 
@@ -101,13 +33,20 @@ overlay.addEventListener('click', onOverlayClick)
 
 
 
+function onEscapeKeyDown(e) {
+  console.log(e.code)
+  if(e.code === 'Escape'){
+    onModalCloseClick()
+  }
+}
+
 function onImgClick(evt){
   evt.preventDefault()
 if(!evt.target.classList.contains('gallery__image')) {
        return 
     }
     modal.classList.add('is-open')
-    window.addEventListener('keydown', onEscapeClick)
+    window.addEventListener('keydown', onEscapeKeyDown)
 
     const originalPisture = evt.target.dataset.source
     modalImg.src = originalPisture
@@ -115,18 +54,13 @@ if(!evt.target.classList.contains('gallery__image')) {
 
 function onModalCloseClick(evt){
   modal.classList.remove('is-open')
-  window.removeEventListener('keydown', onEscapeClick)
+  window.removeEventListener('keydown', onEscapeKeyDown)
 }
 
 function onOverlayClick(){
   onModalCloseClick()
 }
 
-function onEscapeClick(e) {
-  if(e.code === 'Escape'){
-    onModalCloseClick()
-  }
-}
 
 
 document.addEventListener('keydown', onArrowLeftArrowRightClick)
@@ -139,11 +73,11 @@ function onArrowLeftArrowRightClick(e){
       if(newIndex === -1){
         newIndex = ArrayOriginalImgSrc.length -1
       }
-    }
-    else if (e.key === 'ArrowRight'){
+    } else if (e.key === 'ArrowRight'){
       newIndex = curentTd +1;
-      if(newIndex === ArrayOriginalImgSrc.length) 
-        newIndex = 0 
+      if(newIndex === ArrayOriginalImgSrc.length) {
+        newIndex = 0 }
     }
+    console.log(modalImg)
     modalImg.src = ArrayOriginalImgSrc[newIndex]
 }
